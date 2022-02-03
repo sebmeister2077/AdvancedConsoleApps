7d4e92a3056fcbcd4a5c190640326ecd42d780d1
romanunr02 <romanunr02@gmail.com>
2022-02-03 22:55:01 +0200

    created new solution

---
+++ FolderSizes/FolderSizes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FolderSizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
