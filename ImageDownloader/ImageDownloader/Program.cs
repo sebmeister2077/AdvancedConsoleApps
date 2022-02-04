83dcd74ae111a597744f8224bf64a503eea0cf2e
romanunr02 <romanunr02@gmail.com>
2022-02-05 01:20:26 +0200

    convert options dto downloader options

--- ImageDownloader/ImageDownloader/Program.cs
+++ ImageDownloader/ImageDownloader/Program.cs
@@ -10,7 +10,7 @@ namespace WebImageDownloader
         {
             //the characters you want to build a code with
             char[] passChars = GenerateWantedChars();
-            DownloadImages("https://prnt.sc/", @"D:\Images\", "1008lu", "zzzzzz",passChars ,new ExtraOptions());
+            DownloadImages("https://prnt.sc/", @"D:\Images\", "1008lu", "zzzzzz", passChars , new ExtraOptions());
         }
 
         /// <summary>
@@ -23,7 +23,7 @@ namespace WebImageDownloader
 
             while (imageCount < options.MaxAmount)
             {
-                bool downloadSuccess = imgDownloader.DownloadImageFromUrl(downloadUrl + startingValue, savePath);
+                bool downloadSuccess = imgDownloader.DownloadImageFromUrl(downloadUrl + startingValue, savePath,options.ToDownloaderOptions());
 
                 if (downloadSuccess)
                     imageCount++;
@@ -94,6 +94,11 @@ namespace WebImageDownloader
             {
                 this.LogsFilePath = LogsFile;
                 this.MaxAmount = MaxAmount;
+            }
+
+            public ImageDownloaderOptions ToDownloaderOptions()
+            {
+                return new ImageDownloaderOptions(this.LogsFilePath);
             }
         }
 
