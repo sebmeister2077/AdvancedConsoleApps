bac4ac6f8ffce271d2cd2d22cdd4b559070e9e50
romanunr02 <romanunr02@gmail.com>
2022-02-05 00:31:23 +0200

    Added project algorithm

---
+++ ImageDownloader/ImageDownloader/ImageDownloader.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WebImageDownloader
+{
+    public class ImageDownloader
+    {
+        public void DownloadImageFromUrl(string url, string folderImagesPath, string? logFilePath)
+        {
+            var uri = new Uri(url);
+            var pages = new List<HtmlNode> { LoadHtmlDocument(uri) };
+            bool writeLogs = !string.IsNullOrEmpty(logFilePath);
+
+            //pages.AddRange(LoadOtherPages(pages[0], url));    
+            Regex imgReg = new Regex(@"src=\Dhttps:\/\/i\.imgur\.com\/[0-9a-zA-Z]{7}.[a-z]{3}");
+            string data = pages[0].OuterHtml;
+            bool noImageFound = !imgReg.IsMatch(data);
+            if (noImageFound)
+                return;
+
+            string imgurl = imgReg.Match(data).Value.Substring(5);
+            DownloadImage(folderImagesPath, new Uri(imgurl), new WebClient());
+
+            if (writeLogs)
+                WriteToLog(logFilePath, url.Substring(16), imgurl.Substring(20, 7));
+        }
+
+        private static void DownloadImage(string folderImagesPath, Uri url, WebClient webClient)
+        {
+            webClient.DownloadFile(url, Path.Combine(folderImagesPath, Path.GetFileName(url.ToString())));
+        }
+
+        private static IEnumerable<HtmlNode> LoadOtherPages(HtmlNode firstPage, string url)
+        {
+            return Enumerable.Range(1, DiscoverTotalPages(firstPage))
+                             .AsParallel()
+                             .Select(i => LoadHtmlDocument(new Uri(url)));
+        }
+
+        public static void WriteToLog(string logFilePath, string imageCode, string imageName)
+        {
+            StreamWriter stw = new StreamWriter(logFilePath, true);
+            stw.WriteLine(imageCode + ":" + imageName);
+            stw.Dispose();
+        }
+
+        private static int DiscoverTotalPages(HtmlNode documentNode)
+        {
+            var cv = documentNode.SelectNodes("//div[@class='catalogItemList__numsInWiev']");
+            if (cv == null)
+                return 0;
+            var totalItemsDescription = cv.First().InnerText.Trim();
+            var totalItems = int.Parse(Regex.Match(totalItemsDescription, @"\d+$").ToString());
+            var totalPages = (int)Math.Ceiling(totalItems / 50d);
+            return totalPages;
+        }
+
+        private static HtmlNode LoadHtmlDocument(Uri uri)
+        {
+            var doc = new HtmlDocument();
+            var wc = new WebClient();
+            wc.Headers.Add("User-Agent: Other");
+            doc.LoadHtml(wc.DownloadString(uri));
+
+            var documentNode = doc.DocumentNode;
+            return documentNode;
+        }
+    }
+}
---
+++ ImageDownloader/ImageDownloader/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace WebImageDownloader
+{
+    static class Program
+    {
+        //the characters you want to build a string with
+        static char[] passChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        static bool finish = false;
+        static string downloadUrl = "https://prnt.sc/";
+        static string startingValue = "1008lu";
+
+        static void Main(string[] args)
+        {
+            while (!finish)
+            {
+                //Process.Start("chrome.exe", domain + value);
+                //Regex imgReg = new Regex(@"https:\/\/i\.imgur\.com\/[0-9a-zA-Z]{7}.[a-z]{3}");
+                //Console.WriteLine(imgReg.Match("https://i.imgur.com/djB6j0B.png").Success);
+                ImageDownloader imgDownloader = new ImageDownloader();
+                imgDownloader.DownloadImageFromUrl(downloadUrl + startingValue, @"D:\Images\");
+                //WebClient client = new WebClient();
+                //client.DownloadFile(domain + value, basePath+$"image{x}");
+
+                if (startingValue == "zzzzzz")
+                    break;
+                startingValue = GoNext(startingValue);
+            }
+        }
+        static public char NextChar(char c) => passChars[passChars.ToList().IndexOf(c) + 1];
+        static public string GoNext(string text)
+        {
+            char[] str = text.ToCharArray();
+            int charsLen = passChars.Length;
+            int x = text.Length - 1;
+            if (str.All(c => c == passChars[charsLen - 1]))
+            {
+                StringBuilder bob = new StringBuilder();
+                for (int i = 0; i < text.Length + 1; i++)
+                    bob.Append(passChars[0].ToString());
+                return bob.ToString();
+            }
+            while (x >= 0)
+            {
+                if (str[x] != passChars[charsLen - 1])
+                {
+                    str[x] = NextChar(str[x]);
+                    return str.CharsToString();
+                }
+                else
+                    str[x] = passChars[0];
+                x--;
+            }
+            return "";
+        }
+    }
+    public static class StringExtension
+    {
+        public static string CharsToString(this char[] chars)
+        {
+            StringBuilder bob = new StringBuilder();
+            foreach (char c in chars)
+                bob.Append(c);
+            return bob.ToString();
+        }
+    }
+}
