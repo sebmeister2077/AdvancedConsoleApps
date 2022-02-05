5c0dabb86abc7f1b8f1d9ee6c7d4997b0f04fd01
romanunr02 <romanunr02@gmail.com>
2022-02-05 16:11:01 +0200

    Changed logic so its more reusable

--- FolderSizes/FolderSizes/FolderManager.cs
+++ FolderSizes/FolderSizes/FolderManager.cs
@@ -12,25 +12,52 @@ namespace FolderManagerProj
         public List<MyFile> GetBiggerFilesInDirectory(string directoryName, uint minMB, out List<string> unauthorizedAccessFolders)
         {
             List<MyFile> files = new List<MyFile>();
+            Action<FileInfo> fileAction = (FileInfo file) =>
+            {
+                uint size = file.Length.ToMB();
+                if (size > minMB)
+                    files.Add(new MyFile(size, file.Name, file.FullName));
+            };
+
+            Action<DirectoryInfo> directoryAction = (DirectoryInfo directoryInfo) => { };
+            IterateThroughFoldersTree(directoryName, directoryAction, fileAction, out unauthorizedAccessFolders);
+
+            return files;
+        }
+
+        //returns true if all folders have been succesfully deleted, else false and outputs a list of failed deletes
+        public bool RemoveFilesAndFolders(string basePath, ByteSize maxSize)
+            => RemoveFilesAndFolders(basePath, maxSize, out List<string> dummy);
+
+        public bool RemoveFilesAndFolders(string basePath, ByteSize maxSize, out List<string> failedDeletes)
+        {
+            List<MyFile> files = new List<MyFile>();
+            Action<FileInfo> fileAction = (FileInfo file) =>
+            {
+                uint size = file.Length.ToMB();
+            };
+        }
+
+        private void IterateThroughFoldersTree(string directoryName, Action<DirectoryInfo> directoryAction, Action<FileInfo> fileAction, out List<string> exceptionList)
+        {
             Queue<DirectoryInfo> dirQueue = new Queue<DirectoryInfo>();
             Queue<FileInfo> fileQueue = new Queue<FileInfo>();
-            unauthorizedAccessFolders = new List<string>();
-
             dirQueue.Enqueue(new DirectoryInfo(directoryName));
+            exceptionList = new List<string>();
 
             while (dirQueue.Count > 0 || fileQueue.Count > 0)
             {
                 while (fileQueue.Count > 0)
                 {
                     FileInfo file = fileQueue.Dequeue();
-                    uint size = file.Length.ToMB();
-                    if (size > minMB)
-                        files.Add(new MyFile(size, file.Name, file.FullName));
+                    fileAction(file);
                 }
 
                 if (dirQueue.Count > 0)
                 {
                     DirectoryInfo dirInfo = dirQueue.Dequeue();
+                    directoryAction(dirInfo);
+
                     try
                     {
                         DirectoryInfo[] newDirInfos = dirInfo.GetDirectories();
@@ -44,26 +71,10 @@ namespace FolderManagerProj
                     }
                     catch (Exception ex)
                     {
-                        unauthorizedAccessFolders.Add(dirInfo.FullName);
+                        exceptionList.Add(dirInfo.FullName);
                     }
                 }
             }
-
-            return files;
-        }
-
-        //returns true if all folders have been succesfully deleted, else false and outputs a list of failed deletes
-        public bool RemoveFilesAndFolders(string basePath, ByteSize maxSize)
-            => RemoveFilesAndFolders(basePath, maxSize, out List<string> dummy);
-
-        public bool RemoveFilesAndFolders(string basePath, ByteSize maxSize, out List<string> failedDeletes)
-        {
-
-        }
-
-        private void IterateThroughFoldersTree()
-        {
-            //use delegates as params for code reusability
         }
     }
     public class MyFile
@@ -102,11 +113,18 @@ namespace FolderManagerProj
         {
             if (newType == Type)
                 return;
-            if (newType < Type)
+            if (newType > Type)
             {
                 Size = Size.IncrementType();
+                Type++;
+            }
+            else
+            {
+                Size = Size.DecrementType();
+                Type--;
             }
 
+            ChangeType(newType);
         }
     }
 
@@ -124,6 +142,21 @@ namespace FolderManagerProj
 
         public static uint IncrementType(this uint number) => number / 1024;
 
-        public static uint ToMB(this long number) => (uint)(number / (1024 * 1024));
+        public static uint ToByteSize(this long number, SizeType byteSize = SizeType.MegaByte)
+        {
+            switch (byteSize)
+            {
+                case SizeType.Byte:
+                    return (uint)number;
+                case SizeType.KiloByte:
+                    return (uint)number / 1024;
+                case SizeType.MegaByte:
+                    return (uint)(number / (1024 * 1024));
+                case SizeType.GigaByte:
+                    return (uint)((number / (1024 * 1024 * 1024)));
+                default:
+                    return (uint)number;
+            }
+        }
     }
 }
