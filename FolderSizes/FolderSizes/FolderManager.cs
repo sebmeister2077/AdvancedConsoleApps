33c02c5a01332672bd2f12a2f132f543891da2aa
romanunr02 <romanunr02@gmail.com>
2022-02-05 14:02:38 +0200

    Added classes and reorganised solution

---
+++ FolderSizes/FolderSizes/FolderManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FolderManagerProj
+{
+    public class FolderManager
+    {
+        public List<MyFile> GetBiggerFilesInDirectory(string directoryName, uint minMB)
+            => GetBiggerFilesInDirectory(directoryName, minMB, out List<string> dummyList);
+
+        public List<MyFile> GetBiggerFilesInDirectory(string directoryName, uint minMB, out List<string> unauthorizedAccessFolders)
+        {
+            List<MyFile> files = new List<MyFile>();
+            Queue<DirectoryInfo> dirQueue = new Queue<DirectoryInfo>();
+            Queue<FileInfo> fileQueue = new Queue<FileInfo>();
+            unauthorizedAccessFolders = new List<string>();
+
+            dirQueue.Enqueue(new DirectoryInfo(directoryName));
+
+            while (dirQueue.Count > 0 || fileQueue.Count > 0)
+            {
+                while (fileQueue.Count > 0)
+                {
+                    FileInfo file = fileQueue.Dequeue();
+                    uint size = file.Length.ToMB();
+                    if (size > minMB)
+                        files.Add(new MyFile(size, file.Name, file.FullName));
+                }
+
+                if (dirQueue.Count > 0)
+                {
+                    DirectoryInfo dirInfo = dirQueue.Dequeue();
+                    try
+                    {
+                        DirectoryInfo[] newDirInfos = dirInfo.GetDirectories();
+                        FileInfo[] newFiles = dirInfo.GetFiles();
+
+                        foreach (var info in newDirInfos)
+                            dirQueue.Enqueue(info);
+                        foreach (var info in newFiles)
+                            fileQueue.Enqueue(info);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        unauthorizedAccessFolders.Add(dirInfo.FullName);
+                    }
+                }
+            }
+
+            return files;
+        }
+
+        //returns true if all folders have been succesfully deleted, else false and outputs a list of failed deletes
+        public bool RemoveFilesAndFolders(string basePath, ByteSize maxSize)
+            => RemoveFilesAndFolders(basePath, maxSize, out List<string> dummy);
+
+        public bool RemoveFilesAndFolders(string basePath, ByteSize maxSize, out List<string> failedDeletes)
+        {
+
+        }
+
+    }
+    public class MyFile
+    {
+        public uint SizeMB { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullPath { get; set; }
+
+        public MyFile() { }
+
+        public MyFile(uint size, string name, string fullpath)
+        {
+            SizeMB = size;
+            Name = name;
+            FullPath = fullpath;
+        }
+    }
+
+    public class ByteSize
+    {
+        public uint Size { get; set; }
+
+        public SizeType Type { get; set; }
+
+        public ByteSize() { }
+
+        public ByteSize(uint size, SizeType type)
+        {
+            Type = type;
+            Size = size;
+        }
+
+        public void ChangeType(SizeType newType)
+        {
+            if (newType == Type)
+                return;
+            if (newType < Type)
+            {
+                Size = Size.IncrementType();
+            }
+
+        }
+    }
+
+    public enum SizeType
+    {
+        Byte = 1,
+        KiloByte = 2,
+        MegaByte = 3,
+        GigaByte = 4,
+    }
+
+    public static class Extensions
+    {
+        public static uint DecrementType(this uint number) => number * 1024;
+
+        public static uint IncrementType(this uint number) => number / 1024;
+
+        public static uint ToMB(this long number) => (uint)(number / (1024 * 1024));
+    }
+}
--- FolderSizes/FolderSizes/Program.cs
+++ FolderSizes/FolderSizes/Program.cs
@@ -3,101 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Principal;
 
-namespace FolderSizes
+namespace FolderManagerProj
 {
     internal class Program
     {
-
-        public class MyFile
-        {
-            public int SizeMB { get; set; }
-
-            public string Name { get; set; }
-
-            public string FullPath { get; set; }
-
-            public MyFile() { }
-
-            public MyFile(int size, string name, string fullpath)
-            {
-                SizeMB = size;
-                Name = name;
-                FullPath = fullpath;
-            }
-        }
         static void Main(string[] args)
         {
             string folderDir = @"C:\Users\Sebas";
             int minMbB = 900;
+            var manager = new FolderManager();
 
-            var res = GetBiggerFilesInDirectory(folderDir, minMbB);
+            var res = manager.GetBiggerFilesInDirectory(folderDir, minMbB);
         }
-
-        
-
-        static public List<MyFile> GetBiggerFilesInDirectory(string directoryName, int minMB)
-        {
-            List<MyFile> files = new List<MyFile>();
-            Queue<DirectoryInfo> dirQueue = new Queue<DirectoryInfo>();
-            Queue<FileInfo> fileQueue = new Queue<FileInfo>();
-
-            dirQueue.Enqueue(new DirectoryInfo(directoryName));
-
-            while(dirQueue.Count > 0 || fileQueue.Count > 0)
-            {
-                while(fileQueue.Count > 0)
-                {
-                    FileInfo file = fileQueue.Dequeue();
-                    int size = file.Length.ToMB();
-                    if (size > minMB)
-                        files.Add(new MyFile(size, file.Name, file.FullName));
-                }
-
-                if(dirQueue.Count > 0)
-                {
-                    try
-                    {
-                        
-                        DirectoryInfo dirInfo = dirQueue.Dequeue();
-                        DirectoryInfo[] newDirInfos = dirInfo.GetDirectories();
-                        FileInfo[] newFiles = dirInfo.GetFiles();
-
-                        foreach(var info in newDirInfos)
-                            dirQueue.Enqueue(info);
-                        foreach(var info in newFiles)
-                            fileQueue.Enqueue(info);
-
-                    }catch(Exception ex) { }
-                }
-            }
-
-            return files;
-        }
-
-        public bool IsUserAdministrator()
-        {
-            bool isAdmin;
-            try
-            {
-                WindowsIdentity user = WindowsIdentity.GetCurrent();
-                WindowsPrincipal principal = new WindowsPrincipal(user);
-                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                isAdmin = false;
-            }
-            catch (Exception ex)
-            {
-                isAdmin = false;
-            }
-            return isAdmin;
-        }
-
-    }
-    public static class Extensions
-    {
-        public static int ToMB(this long number) => (int)(number / (1024 * 1024));
     }
 
 }
