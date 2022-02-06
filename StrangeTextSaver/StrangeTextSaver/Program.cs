f881fee520b43efb4f724cf07c35d5fa4ac8b9b4
romanunr02 <romanunr02@gmail.com>
2022-02-06 18:33:49 +0200

    added createfile method

--- StrangeTextSaver/StrangeTextSaver/Program.cs
+++ StrangeTextSaver/StrangeTextSaver/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.IO;
 
-namespace StrangeTextSaver
+namespace DataSaver
 {
     internal class Program
     {
         //save text in a png/jpeg or other types of files
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dirPath = @"D:/test";
+            var saver = new StrangeDataSaver();
+
+            string filepath = $"{dirPath}/{"helloworld"}.{"in"}";
+            FileInfo file = new FileInfo(filepath);
+            var stream = file.OpenWrite();
+            //stream.Seek(0, SeekOrigin.Begin);
+            stream.WriteByte(2);
+            stream.Flush();
+            stream.Close();
         }
+    }
+
+    public class StrangeDataSaver
+    {
+
+        public FileStream CreateFile(string dirPath, string fileName, string extension)
+        {
+            DirectoryInfo dir = new DirectoryInfo(dirPath);
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException();
+
+            FileInfo file = new FileInfo($"{dirPath}/{fileName}.{extension}");
+            if (file.Exists)
+                throw new InvalidOperationException();
+
+            return File.Create($"{dirPath}/{fileName}.{extension}");
+        }
+
     }
 }
