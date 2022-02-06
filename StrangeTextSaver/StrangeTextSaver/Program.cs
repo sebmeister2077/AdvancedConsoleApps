dea2fb4d1225202fba1bd58a3e4fd1e26e0a1b1c
romanunr02 <romanunr02@gmail.com>
2022-02-06 17:07:47 +0200

    created project

---
+++ StrangeTextSaver/StrangeTextSaver/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StrangeTextSaver
+{
+    internal class Program
+    {
+        //save text in a png/jpeg or other types of files
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
