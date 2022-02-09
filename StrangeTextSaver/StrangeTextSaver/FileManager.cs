3f27f39b98542bcc05df13136869e3f3e7e70b62
romanunr02 <romanunr02@gmail.com>
2022-02-09 21:46:14 +0200

    added a file manager static class

---
+++ StrangeTextSaver/StrangeTextSaver/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSaver
+{
+    public static class FileManager
+    {
+        public static void MoveHiddenBytesToFile(string pathSource, string pathDestination)
+        {
+            var saver = new DataStream();
+            var data = saver.GetWrittenBytes(pathSource);
+            File.WriteAllBytes(pathDestination, data);
+        }
+
+        public static void AppendBytesToSpecialFile(string bytePathSource, string bytePathDestination)
+        {
+            var saver = new DataStream();
+            var dataToBeAdded = File.ReadAllBytes(bytePathSource);
+            saver.AppendBytesToFile(bytePathDestination, dataToBeAdded);
+        }
+
+    }
+}
