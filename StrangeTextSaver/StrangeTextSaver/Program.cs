f0ff7fa3ca8e63086f0c4561b8e4d51190a7e91b
romanunr02 <romanunr02@gmail.com>
2022-02-12 13:50:54 +0200

    removed code from Main

--- StrangeTextSaver/StrangeTextSaver/Program.cs
+++ StrangeTextSaver/StrangeTextSaver/Program.cs
@@ -6,19 +6,9 @@ namespace DataSaver
 {
     public class Program
     {
-        //save text in a png/jpg/jpeg/gif/mp4 or other types of files without corrupting/changing that previous data
+        //save data in a png/jpg/jpeg/gif/mp4 or other types of files without corrupting/changing that previous data
         static void Main(string[] args)
         {
-            string filepath = $"D:/test/{"test"}.{"mp4"}";
-
-            DataStream saver = new DataStream();
-            byte[] dat = new byte[1000000];
-            dat.Initialize();
-            for (int i = 0; i < 500; i++)
-            {
-                saver.AppendBytesToFile(filepath, dat);
-
-            }
 
         }
     }
